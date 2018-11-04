551efa858f6bad85d2ec966be4193376f69678bc
Hank Stoever <hstove@gmail.com>
2018-11-04 14:09:42 -0800

    attach DB to controller

--- app/controllers/RadiksController.js
+++ app/controllers/RadiksController.js
@@ -5,6 +5,7 @@ const makeModelsController = require('./ModelsController');
 const makeController = (db) => {
   const router = express.Router();
   router.use('/models', makeModelsController(db));
+  router.db = db;
   return router;
 };
 
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const makeModelsController = require('./app/controllers/ModelsController');
 const setupController = require('./app/controllers/RadiksController');
 const { getDB } = require('./app/database/mongodb');
 
-const setup = async (config) => {
+const setup = async (config = {}) => {
   const db = await getDB(config.mongoDBUrl);
   const controller = setupController(db);
   return controller;
