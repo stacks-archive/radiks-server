55ca73be9d3549376e783eb5d1b408a1fd76a5fb
Hank Stoever <hstove@gmail.com>
2019-02-12 20:10:46 -0800

    cleaned up code in StreamingController

--- app/controllers/StreamingController.js
+++ app/controllers/StreamingController.js
@@ -3,19 +3,12 @@ const expressWS = require('express-ws');
 const { STREAM_CRAWL_EVENT } = require('../lib/constants');
 
 module.exports = (db, emitter) => {
-  // changeStream.on('change', (event) => {
-  //   console.log(event);
-  //   emitter.emit(EVENT_NAME, [event]);
-  // });
-
   const StreamingController = express.Router();
   expressWS(StreamingController);
 
-  // const StreamingController = expressWS(express.Router());
 
-  StreamingController.ws('/', (ws, req) => {
+  StreamingController.ws('/', (ws) => {
     const listener = ([attributes]) => {
-      console.log('attrs for streaming', attributes);
       ws.send(JSON.stringify(attributes), (error) => {
         console.error(error);
       });
@@ -26,9 +19,6 @@ module.exports = (db, emitter) => {
     ws.on('close', () => {
       emitter.removeListener(STREAM_CRAWL_EVENT, listener);
     });
-    // emitter.addListener(EVENT_NAME, (event) => {
-    //   ws.send(event);
-    // });
   });
 
   return StreamingController;
