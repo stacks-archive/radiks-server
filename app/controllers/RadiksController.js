1d4e75cd681727cd625cca4edc935c8208aa5a95
Hank Stoever <hstove@gmail.com>
2019-02-18 23:21:17 -0800

    adds Central API

--- app/controllers/RadiksController.js
+++ app/controllers/RadiksController.js
@@ -3,9 +3,16 @@ const EventEmitter = require('wolfy87-eventemitter');
 
 const makeModelsController = require('./ModelsController');
 const makeStreamingController = require('./StreamingController');
+const makeCentralController = require('./CentralController');
+
+const COLLECTION = 'radiks-server-data';
+const CENTRAL_COLLECTION = 'radiks-central-data';
 
 const makeController = (db) => {
   const router = express.Router();
+
+  const radiksCollection = db.collection(COLLECTION);
+  const centralCollection = db.collection(CENTRAL_COLLECTION);
 
   router.use((req, res, _next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -15,11 +22,16 @@ const makeController = (db) => {
 
   const emitter = new EventEmitter();
 
-  router.use('/models', makeModelsController(db, emitter));
+  router.use('/models', makeModelsController(radiksCollection, emitter));
 
-  router.use('/stream', makeStreamingController(db, emitter));
+  router.use('/stream', makeStreamingController(radiksCollection, emitter));
 
-  router.db = db;
+  router.use('/central', makeCentralController(radiksCollection, centralCollection));
+
+  router.db = radiksCollection; // for backwards compatibility
+  router.DB = db;
+  router.radiksCollection = radiksCollection;
+  router.centralCollection = centralCollection;
 
   return router;
 };
--- app/database/mongodb.js
+++ app/database/mongodb.js
@@ -1,14 +1,12 @@
 const { MongoClient } = require('mongodb');
 
-const COLLECTION = 'radiks-server-data';
-
 const getDB = url => new Promise(async (resolve, reject) => {
   const _url = url || process.env.MONGODB_URI || 'mongodb://localhost:27017/radiks-server';
   MongoClient.connect(_url, { useNewUrlParser: true }, (err, client) => {
     if (err) {
       return reject(err);
     }
-    return resolve(client.db().collection(COLLECTION));
+    return resolve(client.db());
   });
 });
 
