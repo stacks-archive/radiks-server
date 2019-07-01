a12bf76f36f7210e997ac5136e177746c9a246ac
pradel <pradel.leo@gmail.com>
2019-07-01 10:00:55 +0200

    Eslint config typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,15 @@
 module.exports = {
-  extends: 'airbnb-base',
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
   env: {
     'jest/globals': true,
   },
