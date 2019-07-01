d585cad97159f59d83c74ba0ae9830dbf2cd4778
pradel <pradel.leo@gmail.com>
2019-07-01 09:32:14 +0200

    comvert database

---
+++ src/database/mongodb.ts
@@ -0,0 +1,15 @@
+import { MongoClient } from "mongodb";
+
+export const getDB = (url?: string) =>
+  new Promise(async (resolve, reject) => {
+    const _url =
+      url ||
+      process.env.MONGODB_URI ||
+      "mongodb://localhost:27017/radiks-server";
+    MongoClient.connect(_url, { useNewUrlParser: true }, (err, client) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(client.db());
+    });
+  });
