da89fd768d23dc0fdae97bfd358182df4bc33325
Hank Stoever <hstove@gmail.com>
2019-07-23 19:25:42 -0700

    eslint for semis

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     'no-console': [0],
     'new-cap': [0],
     'import/no-unresolved': [0],
+    semi: [2, 'always'],
     '@typescript-eslint/explicit-function-return-type': [0],
     '@typescript-eslint/no-explicit-any': [0],
     '@typescript-eslint/explicit-member-accessibility': [0],
