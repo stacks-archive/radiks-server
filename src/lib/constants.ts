f990e3008e784b377eb8533ecf6dd96b691a8cdf
Hank Stoever <hstove@gmail.com>
2019-07-25 08:09:33 -0600

    linting error

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,6 +2,6 @@ const constants = {
   STREAM_CRAWL_EVENT: 'STREAM_CRAWL_EVENT',
   COLLECTION: 'radiks-server-data',
   CENTRAL_COLLECTION: 'radiks-central-data',
-}
+};
 
 export default constants;
